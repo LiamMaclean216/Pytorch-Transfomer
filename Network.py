b4d4df0a0c4b0037e867d6166342ebd67a631628
Liam Maclean <lmaclean234@gmail.com>
2020-06-26 15:37:24 -0400

    Multi-head attention

--- Network.py
+++ Network.py
@@ -6,10 +6,12 @@ from utils import *
 
 
 class EncoderLayer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn):
+    def __init__(self, dim_val, dim_attn, n_heads = 1):
         super(EncoderLayer, self).__init__()
-        self.attn = AttentionBlock(dim_val, dim_attn)
+        self.attn = MultiHeadAttentionBlock(dim_val, dim_attn , n_heads)
         self.fc1 = nn.Linear(dim_val, dim_val)
+        self.fc2 = nn.Linear(dim_val, dim_val)
+        
         self.norm1 = nn.LayerNorm(dim_val)
         self.norm2 = nn.LayerNorm(dim_val)
     
@@ -17,16 +19,16 @@ class EncoderLayer(torch.nn.Module):
         a = self.attn(x)
         x = self.norm1(x + a)
         
-        a = self.fc1(x)
+        a = self.fc1(F.elu(self.fc2(x)))
         x = self.norm2(x + a)
         
         return x
 
 class DecoderLayer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn):
+    def __init__(self, dim_val, dim_attn, n_heads = 1):
         super(DecoderLayer, self).__init__()
-        self.attn1 = AttentionBlock(dim_val, dim_attn)
-        self.attn2 = AttentionBlock(dim_val, dim_attn)
+        self.attn1 = MultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
+        self.attn2 = MultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
         self.fc1 = nn.Linear(dim_val, dim_val)
         self.fc2 = nn.Linear(dim_val, dim_val)
         
@@ -41,51 +43,44 @@ class DecoderLayer(torch.nn.Module):
         a = self.attn2(x, kv = enc)
         x = self.norm2(a + x)
         
-        a = self.fc1(x)
-        a = self.fc2(x)
-        a = F.relu(x)
+        a = self.fc1(F.elu(self.fc2(x)))
+        
         x = self.norm3(x + a)
         return x
 
 class Transformer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn, input_size, input_seq_len, output_seq_len, n_decoder_layers = 1, n_encoder_layers = 1):
+    def __init__(self, dim_val, dim_attn, input_size, dec_seq_len, out_seq_len, n_decoder_layers = 1, n_encoder_layers = 1, n_heads = 1):
         super(Transformer, self).__init__()
-        self.output_seq_len = output_seq_len
+        self.dec_seq_len = dec_seq_len
         
         #Initiate encoder and Decoder layers
         self.encs = []
         for i in range(n_encoder_layers):
-            self.encs.append(EncoderLayer(dim_val, dim_attn))
+            self.encs.append(EncoderLayer(dim_val, dim_attn, n_heads))
         
         self.decs = []
         for i in range(n_decoder_layers):
-            self.decs.append(DecoderLayer(dim_val, dim_attn))
+            self.decs.append(DecoderLayer(dim_val, dim_attn, n_heads))
         
         self.pos = PositionalEncoding(dim_val)
         
         #Dense layers for managing network inputs and outputs
         self.enc_input_fc = nn.Linear(input_size, dim_val)
         self.dec_input_fc = nn.Linear(input_size, dim_val)
-        self.out_fc = nn.Linear(output_seq_len * dim_val, input_size)
+        self.out_fc = nn.Linear(dec_seq_len * dim_val, out_seq_len)
     
     def forward(self, x):
         #encoder
-        e = self.enc_input_fc(x)
-       # e = torch.zeros(e.shape)
-        #print(x.shape, e.shape)
-        e = self.encs[0](self.pos(e))
+        e = self.encs[0](self.pos(self.enc_input_fc(x)))
         for enc in self.encs[1:]:
             e = enc(e)
         
         #decoder
-        #print(x[:,-self.output_seq_len:].shape)
-        d = self.decs[0](self.dec_input_fc(x[:,-self.output_seq_len:]), e)
+        d = self.decs[0](self.dec_input_fc(x[:,-self.dec_seq_len:]), e)
         for dec in self.decs[1:]:
             d = dec(d, e)
             
-            #print(d.shape)
         #output
-        #print(d.shape)
         x = self.out_fc(d.flatten(start_dim=1))
         
         return x
--- utils.py
+++ utils.py
@@ -4,29 +4,62 @@ import torch.nn.functional as F
 import numpy as np
 import math
 
-def attention(Q, K, V):
-    #Attention(Q, K, V) = norm(QK)V
-    
-    a = a_norm(Q, K)
-    #print(Q.shape, K.shape, a.shape)
-    
-    a = torch.matmul(a,  V)
-    #print(Q.shape, K.shape, V.shape, a.shape)
-    #print()
-    return a
-
 def a_norm(Q, K):
-    
     m = torch.matmul(Q, K.transpose(2,1).float())
-    #print(m/ torch.sqrt(torch.tensor(dim_attn).float()))
+    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())
+    
     return torch.softmax(m , -1)
 
+
+def attention(Q, K, V):
+    #Attention(Q, K, V) = norm(QK)V
+    a = a_norm(Q, K) #(batch_size, dim_attn, seq_length)
+    
+    return  torch.matmul(a,  V) #(batch_size, seq_length, seq_length)
+
+class AttentionBlock(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn):
+        super(AttentionBlock, self).__init__()
+        self.value = Value(dim_val, dim_val)
+        self.key = Key(dim_val, dim_attn)
+        self.query = Query(dim_val, dim_attn)
+    
+    def forward(self, x, kv = None):
+        if(kv is None):
+            #Attention with x connected to Q,K and V (For encoder)
+            return attention(self.query(x), self.key(x), self.value(x))
+        
+        #Attention with x as Q, external vector kv as K an V (For decoder)
+        return attention(self.query(x), self.key(kv), self.value(kv))
+    
+class MultiHeadAttentionBlock(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn, n_heads):
+        super(MultiHeadAttentionBlock, self).__init__()
+        self.heads = []
+        for i in range(n_heads):
+            self.heads.append(AttentionBlock(dim_val, dim_attn))
+        
+        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias = False)
+                      
+        
+    def forward(self, x, kv = None):
+        a = []
+        for h in self.heads:
+            a.append(h(x, kv = kv))
+            
+        a = torch.stack(a, dim = -1) #combine heads
+        a = a.flatten(start_dim = 2) #flatten all head outputs
+        
+        x = self.fc(a)
+        
+        return x
+    
 class Value(torch.nn.Module):
     def __init__(self, dim_input, dim_val):
         super(Value, self).__init__()
         self.dim_val = dim_val
         
-        self.fc1 = nn.Linear(dim_input, dim_val)
+        self.fc1 = nn.Linear(dim_input, dim_val, bias = False)
         #self.fc2 = nn.Linear(5, dim_val)
     
     def forward(self, x):
@@ -40,7 +73,7 @@ class Key(torch.nn.Module):
         super(Key, self).__init__()
         self.dim_attn = dim_attn
         
-        self.fc1 = nn.Linear(dim_input, dim_attn)
+        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
         #self.fc2 = nn.Linear(5, dim_attn)
     
     def forward(self, x):
@@ -54,7 +87,7 @@ class Query(torch.nn.Module):
         super(Query, self).__init__()
         self.dim_attn = dim_attn
         
-        self.fc1 = nn.Linear(dim_input, dim_attn)
+        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
         #self.fc2 = nn.Linear(5, dim_attn)
     
     def forward(self, x):
@@ -64,22 +97,6 @@ class Query(torch.nn.Module):
         #x = self.fc2(x)
         
         return x
-
-    
-class AttentionBlock(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn):
-        super(AttentionBlock, self).__init__()
-        self.value = Value(dim_val, dim_val)
-        self.key = Key(dim_val, dim_attn)
-        self.query = Query(dim_val, dim_attn)
-    
-    def forward(self, x, kv = None):
-        if(kv is None):
-            #Attention with x connected to Q,K and V (For encoder)
-            return attention(self.query(x), self.key(x), self.value(x))
-        
-        #Attention with x as Q, external vector kq as K an V (For decoder)
-        return attention(self.query(x), self.key(kv), self.value(kv))
 
 # https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 class PositionalEncoding(nn.Module):
